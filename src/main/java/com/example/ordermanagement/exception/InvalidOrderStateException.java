3565c2a414555aff346d8bda0ea0d0c8db5b3472
somanathkambar <kambarsomanath@gmai.com>
2025-12-27 11:18:56 -0800

    Adding Custom exceptions and a global exception handler

---
+++ src/main/java/com/example/ordermanagement/exception/InvalidOrderStateException.java
@@ -0,0 +1,11 @@
+package com.example.ordermanagement.exception;
+
+public class InvalidOrderStateException extends RuntimeException {
+    public InvalidOrderStateException(String message) {
+        super(message);
+    }
+    
+    public InvalidOrderStateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
